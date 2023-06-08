e4d047886c51917c5154e8aca248322cb9d83e27
Dhiraj999 <dhirajs@valueaddsofttech.com>
2023-06-08 18:42:07 +0530

    created doctypefieldSlice

--- dmsfrontend/src/store/store.js
+++ dmsfrontend/src/store/store.js
@@ -1,5 +1,7 @@
 import { create } from "zustand";
 import { createDepartmentSlice } from "./slices/createDepartmentSlice";
+import { createDocTypeFieldSlice } from "./slices/createDocTypeFieldSlice";
 export const useBoundStore = create((...a) => ({
   ...createDepartmentSlice(...a),
+  ...createDocTypeFieldSlice(...a),
 }));
